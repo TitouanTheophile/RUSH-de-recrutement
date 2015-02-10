15f5e313cd60265427702d7080f2da39ef16f3bd
TitouanTheophile <titouan.theophile@epitech.eu>
2015-02-10 13:03:08 +0100

    ./Model/AppModel.php

--- app/Model/Profile.php
+++ app/Model/Profile.php
@@ -1,6 +1,19 @@
 <?php
 
 class Profile extends AppModel {
+	function beforeFind($query)
+	{
+		if (preg_match("/^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,}$/i",$query['conditions']['email']) == false)
+			return false;
+		return $query;
+	}
 	
+	function afterFind($answer, $bool = false)
+	{
+		if (!empty($answer))
+		return $answer;
+		else
+		return NULL;
+	}
 }
 ?>
