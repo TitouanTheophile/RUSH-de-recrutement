b853a22839a80589c8e6b8f02d62ef93efa5488b
toto <toto@toto.toto>
2015-02-23 16:14:12 +0100

    Fixed searchbar

--- app/webroot/js/dynamic_search.js
+++ app/webroot/js/dynamic_search.js
@@ -1,26 +1,13 @@
 $(document).ready(function() {
-
-
+  
   $('#ProfileId').keyup( function() {
-    if( $(this).val().length > 1 ) {
-      url = '/users/get_users';
-      $.ajax( {
-        type : 'GET',
-        url : url ,
-        data : 'q='+$(this).val() ,
-        success : function(data) {
-          $('#results_search').html(data);
-        }
-      });
-      url = '/groups/get_groups';
-      $.ajax( {
-        type : 'GET',
-        url : url ,
-        data : 'q='+$(this).val() ,
-        success : function(data) {
-          $('#results_search').append(data);
-        }
-      });
+    if ($(this).val().length > 1) {
+      $.when(getResults('/users/get_users?q=', $(this).val()),
+             getResults('/groups/get_groups?q=', $(this).val())
+            ).done(function(users, groups) {
+                var data = users[0] + groups[0];
+                $('#results_search').html(data);
+            });
     }
     else {
       $('#results_search').html('');
@@ -35,4 +22,10 @@ $(document).ready(function() {
   $('#ProfileId').on('click', function() {
         event.stopPropagation();
   })
+ 
+var getResults = function(url, query) {
+    var url = url + query;
+    return $.get(url);
+};
+
 });
